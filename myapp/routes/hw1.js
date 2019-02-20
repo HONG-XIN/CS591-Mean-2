7fba455ebf6559ea696e7be6492c56eac4133094
dezhouw <hongsinwang@gmail.com>
2019-02-20 18:42:16 -0500

    modify hw1.js to return json

--- myapp/routes/hw1.js
+++ myapp/routes/hw1.js
@@ -3,11 +3,13 @@ var router = express.Router();
 
 /* GET listing. */
 router.get('/', function(req, res, next) {
-  res.send('GET');
+  res.send({
+  	router: 'hw1',
+  	method: 'GET'});
 });
 
 router.post('/', function(req, res, next) {
-  res.send('POST');
+  res.send({method: 'POST'});
 });
 
 module.exports = router;
