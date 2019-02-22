c1da80e7a312de701381fb43034ec0fc54f46f9e
dezhouw <hongsinwang@gmail.com>
2019-02-22 16:49:38 -0500

    add post /foo to index.js

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/foo', function(req, res, next) {
+  res.render('index', { title: 'Hello FOO!' });
+})
+
 module.exports = router;
